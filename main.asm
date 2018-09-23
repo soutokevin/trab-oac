35d5b37068ff4e10a6e424cacd8d5a2b3d1e8043
Kevin Souto <soutokevin@gmail.com>
2018-09-23 11:50:15 -0300

    Read image to mars bitmap display

--- main.asm
+++ main.asm
@@ -1,9 +1,12 @@
 .data
+  screen: .space 1048576 # Reserve space for the bitmap display at 512x512
   open_error_msg: .asciiz "\nError while opening file\n"
   error_msg: .asciiz "\nInvalid file\n"
   path: .asciiz "images/lena.bmp"
+  width: .word 0
+  height: .word 0
   file: .word 0
-  header: .space 40
+  buffer: .space 1536
 
 .text
   la $a0, path # set image path
@@ -17,7 +20,7 @@
 
   # Load file header info
   move $a0, $v0
-  la $a1, header
+  la $a1, buffer
   li $a2, 14
   li $v0, 14
   syscall
@@ -38,26 +41,53 @@
   li $v0, 14
   syscall
 
-  la $s0, header             # Load header base address
+  la $s0, buffer             # Load header base address
 
   lw $t0, 0($s0)             # Load actual header size
   bne $t0, 40, invalid_file  # Header size must be 40
 
   lw $a0, 4($s0)             # Load image width
-  li $v0, 1                  # Print Integer syscall
-  syscall                    # Print image width
-
-  # Print a single 'x' char
-  li $a0, 'x'
-  li $v0, 11
-  syscall
+  sw $a0, width              # Store image width
 
   lw $a0, 8($s0)             # Load image height
-  li $v0, 1                  # Print Integer syscall
-  syscall                    # Print image height
+  sw $a0, height             # Store image height
 
   lhu $t0, 14($s0)           # Load pixel size in bits
   bne $t0, 24, invalid_file  # Only 24-bit images are supported
+
+  la $s0, screen             # s0 is the base address of the screen
+  lw $s1, height             # s1 will count how many lines are left to paint
+
+paint_line:
+  # Load 1536 bytes (a full line) of pixel data
+  lw $a0, file
+  la $a1, buffer
+  li $a2, 1536
+  li $v0, 14
+  syscall
+
+  la $t8, buffer             # Start of loaded file content
+  la $t9, buffer + 1536      # End of loaded file content
+
+paint_pixel:
+  lbu $t0, 2($t8)            # Load red component
+  lbu $t1, 1($t8)            # Load green component
+  lbu $t2, 0($t8)            # Load blue component
+
+  sll $t0, $t0, 16           # Prepare component to be joined; red   <<= 16
+  sll $t1, $t1, 8            # Prepare component to be joined; green <<=  8
+
+  or $t0, $t0, $t1           # t0 contains red and green components
+  or $t0, $t0, $t2           # t0 contains all rgb components
+
+  sw $t0, 0($s0)             # Paint pixel
+
+  addi $s0, $s0, 4           # Update screen address
+  addi $t8, $t8, 3           # Update file address
+  blt $t8, $t9, paint_pixel  # Are we done with this line?
+
+  addi $s1, $s1, -1          # Finished painting one line, decrement s1
+  bnez $s1, paint_line       # Are we done yet?
 
 exit:
   li $v0, 10
